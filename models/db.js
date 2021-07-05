51affa215759741edb35305ebde21bd8ba373726
Roberto de Oliveira Faria <robertoliveirafaria@gmail.com>
2021-07-05 13:48:47 -0300

    removendo autenticação mongo

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://medidorco2:rdapdm22@0.0.0.0/medidor?authSource=admin');
+mongoose.connect('mongodb://0.0.0.0/medidor');
 
 const ambiente = new Schema({
     nome: { type: String, default: '' },
